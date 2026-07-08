8b54136c1eb3a4924e222333ad3a8034238e1e7f
khaledhajeid <khaled1.hajeid@gmail.com>
2026-07-08 16:03:24 +0300

    fix the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // 🟢 تعريف خط Tajawal كخط أساسي للتطبيق بالكامل
+      fontFamily: {
+        sans: ['Tajawal', 'sans-serif'], 
+      },
+    },
   },
   plugins: [],
 }
